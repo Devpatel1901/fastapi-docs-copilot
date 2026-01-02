86cfde03ad4abd11406d83489b7ba4e490d72d9b
Devpatel1901 <entrepreneurdev1901@gmail.com>
2026-01-02 12:14:03 -0500

    Updated system prompt and added summarization middleware

--- middleware.py
+++ middleware.py
@@ -1,11 +1,5 @@
 from langchain.agents.middleware import dynamic_prompt, ModelRequest
 from vecter_store import retrieve_similar_documents
-from langchain.agents.middleware import before_model
-from langchain.agents import AgentState
-from langgraph.runtime import Runtime
-from langchain.messages import RemoveMessage
-from typing import Any
-from langgraph.graph.message import REMOVE_ALL_MESSAGES
 
 @dynamic_prompt
 def prompt_with_context(request: ModelRequest) -> str:
@@ -25,27 +19,11 @@ def prompt_with_context(request: ModelRequest) -> str:
         "As well as cite the source of the information from the context."
         "You can generate web page url by appending the source path to 'https://fastapi.tiangolo.com/' "
         "at start and only picking last two values of path from metadata source attribute."
-        "For example, if source is './docs_data/docs/tutorial/body-multiple-params#body-multiple-parameters' then the url will be:"
-        "'https://fastapi.tiangolo.com/tutorial/body-multiple-params#body-multiple-parameters'.\n\n"
+        "For example,"
+        "1. source: './docs_data/docs/tutorial/body-multiple-params#body-multiple-parameters'"
+        "   url: 'https://fastapi.tiangolo.com/tutorial/body-multiple-params/#body-multiple-parameters'." \
+        "2. source: './docs_data/docs/python-types/#declaring-types'" \
+        "   url: 'https://fastapi.tiangolo.com/python-types/#declaring-types'.\n\n"
         f"Context:\n{context}\n\n"
     )
     return system_message
-
-@before_model
-def trim_messages(state: AgentState, runtime: Runtime) -> dict[str, Any] | None:
-    """Keep only the last few messages to fit context window."""
-    messages = state["messages"]
-
-    if len(messages) <= 3:
-        return None  # No changes needed
-
-    first_msg = messages[0]
-    recent_messages = messages[-3:] if len(messages) % 2 == 0 else messages[-4:]
-    new_messages = [first_msg] + recent_messages
-
-    return {
-        "messages": [
-            RemoveMessage(id=REMOVE_ALL_MESSAGES),
-            *new_messages
-        ]
-    }
--- rag.py
+++ rag.py
@@ -1,9 +1,11 @@
 from dotenv import load_dotenv
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain.agents import create_agent
+from langchain.agents.middleware import SummarizationMiddleware
 from langchain.messages import HumanMessage
 from langgraph.checkpoint.memory import InMemorySaver
-from middleware import prompt_with_context, trim_messages
+from middleware import prompt_with_context
+from langchain_core.messages.ai import AIMessageChunk
 
 load_dotenv()
 
@@ -15,20 +17,33 @@ def initiate_qa_bot():
         model=llm,
         tools=[],
         checkpointer=InMemorySaver(),
-        middleware=[trim_messages, prompt_with_context],
+        middleware=[
+            prompt_with_context,
+            SummarizationMiddleware(
+                model=llm,
+                trigger=("fraction", 0.7), # gemini-3-pro-preview has 1M token context window
+                keep=("fraction", 0.3)
+            )
+        ],
     )
 
     while input_query.lower() != "exit":
         input_query = input("Enter your question about FastAPI docs (or type 'exit' to quit): ")
+        full_message = ""
         if input_query.lower() == "exit":
             break
 
-        for step in agent.stream(
+        for token, _ in agent.stream(
             {"messages": [HumanMessage(input_query)]},
             config={"configurable": {"thread_id": "1"}},
-            stream_mode="values"
+            stream_mode="messages"
         ):
-            step["messages"][-1].pretty_print()
+            if isinstance(token, AIMessageChunk):
+                if token.chunk_position != "last":  
+                    full_message += token.content[0]["text"]
+
+        print("\nAI Response:")
+        print(full_message)
 
 if __name__ == "__main__":
     initiate_qa_bot()
